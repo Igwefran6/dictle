546db6058bd6dc934f6fcd3a8c1e8126db5b93f9
Igwe Francis <Igwefran6.dev@gmail.com>
2024-05-22 22:22:10 +0100

    --allow-empty

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Check if the environment is production
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/dictle/'
+  base: isProduction ? '/dictle/' : '/'
 });
