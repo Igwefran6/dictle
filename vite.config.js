0a8435f01ebd57a778227e75767b4c5b6f6bed4a
Igwe Francis <Igwefran6.dev@gmail.com>
2024-05-26 11:28:48 +0100

    Configure Vite for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // Check if the environment is production
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  plugins: [react()],
-  base: isProduction ? '/dictle/' : '/'
+    plugins: [react()],
+    base: isProduction ? "/dictle/" : "/",
+    mode: isProduction ? "production" : "development"
 });
