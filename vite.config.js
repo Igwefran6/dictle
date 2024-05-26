20ad55da7c5fce0cd3dfd31f19230868a1763498
Igwe Francis <Igwefran6.dev@gmail.com>
2024-05-26 12:24:52 +0100

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/" // Ensure this is set correctly for GitHub Pages
+    base: "/dictle/" // Ensure this is set correctly for GitHub Pages
 });
