5b8578cac4e05292fe1ace5adbc9defd76df9509
Igwe Francis <Igwefran6.dev@gmail.com>
2024-06-03 14:23:27 +0100

    fix: package.json and vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
     return {
-        plugins: [react()],
-        base: "/dictle/"
+        plugins: [react()]
     };
 });
