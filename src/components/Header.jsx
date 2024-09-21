7d612522f4f510983dae68374715b62dfb93edea
Igwe Francis <igwefran6@gmail.com>
2024-09-21 11:45:41 +0100

    Update UI components and styles: Modified App, Header, SearchBar, and index.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,55 +1,150 @@
-import Icon from "@mdi/react";
-import { mdiBookAlphabet } from "@mdi/js";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
+const LinkButton = ({ text, path = "#", handleClick, extraStyles }) => {
+  return (
+    <li
+      className={
+        `relative hover:scale-105 transition-all cursor-pointer z-10 no-copy px-1 ` +
+        extraStyles
+      }
+      onClick={(e) => {
+        if (handleClick) {
+          handleClick(e);
+        }
+      }}
+    >
+      <Link to={path}>{text}</Link>
+    </li>
+  );
+};
+
 const Header = ({ setShowContactPage }) => {
-    const handleClick = e => {
-        e.preventDefault();
-        setShowContactPage(prev => !prev);
+  const [learningTools, setLearningTools] = useState(false);
+  const [project, setProject] = useState(false);
+  const learningToolsRef = useRef(null);
+  const projectRef = useRef(null);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setShowContactPage((prev) => !prev);
+  };
+
+  const handleOpenLearningTools = () => {
+    setLearningTools((prev) => !prev);
+    setProject(false);
+  };
+
+  const handleProject = () => {
+    setProject((prev) => !prev);
+    setLearningTools(false);
+  };
+
+  // Close dropdowns when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        learningToolsRef.current &&
+        !learningToolsRef.current.contains(event.target)
+      ) {
+        setLearningTools(false);
+      }
+      if (projectRef.current && !projectRef.current.contains(event.target)) {
+        setProject(false);
+      }
     };
-    return (
-        <header className="bg-blue-500 dark:bg-gray-900 text-white p-6 pb-4">
-            <h1 className="text-2xl font-bold flex">
-                <Icon path={mdiBookAlphabet} size="26px" />
-                Dictle.
-            </h1>
-            <nav>
-                <ul className="flex space-x-4">
-                    <li>
-                        {
-                            <Link
-                                to="/"
-                                href="#home"
-                                className="hover:underline hover:font-bold"
-                            >
-                                Home
-                            </Link>
-                        }
-                    </li>
-                    <li>
-                        {
-                            <Link
-                                to="/about"
-                                href="#about"
-                                className="hover:underline hover:font-bold "
-                            >
-                                About
-                            </Link>
-                        }
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            className="hover:underline hover:font-bold"
-                            onClick={e => handleClick(e)}
-                        >
-                            Developer's contact
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <header className="bg-blue-500 dark:bg-gray-900 text-white p-6 pb-4 flex items-baseline gap-8 relative">
+      <h1 className="text-2xl font-bold flex ">Dictle.</h1>
+      <nav>
+        <ul className="flex space-x-4 font-semibold">
+          <LinkButton text="Home" path="/" />
+          <div ref={learningToolsRef}>
+            <LinkButton
+              text="Learning tools"
+              path="/"
+              extraStyles="dropdown"
+              handleClick={handleOpenLearningTools}
+            />
+            <span
+              className={
+                `absolute w-48 bg-white rounded-lg p-4 flex-col gap-1 ` +
+                (learningTools ? "flex" : "hidden")
+              }
+            >
+              <Link
+                to="/"
+                className="w-full text-white bg-slate-500 block rounded-lg px-2 py-1 hover:scale-105 transition-all"
+              >
+                Quiz
+              </Link>
+              <Link
+                to="/"
+                className="w-full text-white bg-slate-500 block rounded-lg px-2 py-1 hover:scale-105 transition-all"
+              >
+                Vocabulary tool
+              </Link>
+              <Link
+                to="/"
+                className="w-full text-white bg-slate-500 block rounded-lg px-2 py-1 hover:scale-105 transition-all"
+              >
+                English Synonyms
+              </Link>
+              <Link
+                to="/"
+                className="w-full text-white bg-slate-500 block rounded-lg px-2 py-1 hover:scale-105 transition-all"
+              >
+                English Antonyms
+              </Link>
+            </span>
+          </div>
+
+          <div ref={projectRef}>
+            <LinkButton
+              text="Project"
+              path="/"
+              extraStyles="dropdown"
+              handleClick={handleProject}
+            />
+            <span
+              className={
+                `absolute w-48 bg-white rounded-lg p-4 flex-col gap-1 ` +
+                (project ? "flex" : "hidden")
+              }
+            >
+              <Link
+                to="/"
+                className="w-full text-white bg-slate-500 block rounded-lg px-2 py-1 hover:scale-105 transition-all"
+              >
+                Learn more
+              </Link>
+              <Link
+                to="/"
+                className="w-full text-white bg-slate-500 block rounded-lg px-2 py-1 hover:scale-105 transition-all"
+              >
+                About project
+              </Link>
+              <Link
+                to="/"
+                className="w-full text-white bg-slate-500 block rounded-lg px-2 py-1 hover:scale-105 transition-all"
+              >
+                Bla bla bla
+              </Link>
+            </span>
+          </div>
+
+          <LinkButton text="Contact" handleClick={handleClick} />
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import Icon from "@mdi/react";
 import { mdiMagnify } from "@mdi/js";
 
-const SearchBar = props => {
-    return (
-        <div className="flex items-center border-b border-gray-300 dark:border-gray-500 dark:bg-gray-600 p-4 py-3">
-            <Icon
-                path={mdiMagnify}
-                size={1}
-                className="text-gray-500 dark:text-gray-400 mr-3"
-            />
-            <form onSubmit={e => props.handleSearch(e)}>
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    value={props.word}
-                    onChange={e => props.setWord(e.target.value)}
-                    className="appearance-none bg-transparent dark:text-gray-100 border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                    ref={props.inputRef}
-                />
-            </form>
-        </div>
-    );
+const SearchBar = (props) => {
+  return (
+    <div className="flex items-center border-b border-gray-300 dark:border-gray-500 dark:bg-gray-600 p-4 py-3">
+      <Icon
+        path={mdiMagnify}
+        size={1}
+        className="text-gray-500 dark:text-gray-400 mr-3"
+      />
+      <form onSubmit={(e) => props.handleSearch(e)}>
+        <input
+          type="text"
+          placeholder="Search word here..."
+          value={props.word}
+          onChange={(e) => props.setWord(e.target.value)}
+          className="appearance-none bg-transparent dark:text-gray-100 border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          ref={props.inputRef}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
